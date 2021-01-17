9a80d1088fa91b7bc82e9ad88218924f959b7ca6
Bruno Garcia <bruno@brunogarcia.com>
2021-01-16 19:20:07 -0500

    code added upstream already

--- Simbolo/Client.cs
+++ Simbolo/Client.cs
@@ -128,7 +128,7 @@ namespace Simbolo
                 return null;
             }
             
-            // Framework's assemblies don't have pdb checksum. I.e: System.Private.CoreLib.pdb
+            // Framework's assemblies don't have pdb checksum. I.e: System.Private.CoreLib.dll
             IEnumerable<string>? checksums = null;
             var pdbChecksum = peReader.ReadDebugDirectory()
                 .FirstOrDefault(d => d.Type == DebugDirectoryEntryType.PdbChecksum);
@@ -147,11 +147,10 @@ namespace Simbolo
                 checksums = new[] {builder.ToString()};
             }
 
-
             var data = peReader.ReadCodeViewDebugDirectoryData(codeView);
             var isPortable = codeView.IsPortableCodeView;
 
-            var signature = data.Guid; // TODO: Always the same as mvid??
+            var signature = data.Guid;
             var age = data.Age;
             var file = data.Path;
 
@@ -163,64 +162,6 @@ namespace Simbolo
                 signature,
                 age,
                 checksums);
-        }
-        
-        // https://github.com/dotnet/runtime/blob/c985bdcec2a9190e733bcada413a193d5ff60c0d/src/libraries/System.Private.CoreLib/src/System/Diagnostics/StackTrace.cs#L375-L430
-        private static bool TryResolveStateMachineMethod(ref MethodBase method, [NotNullWhen(true)] out Type declaringType)
-        {
-            if (method.DeclaringType is null)
-            {
-                declaringType = null!;
-                return false;
-            }
-            declaringType = method.DeclaringType;
-
-            var parentType = declaringType.DeclaringType;
-            if (parentType is null)
-            {
-                return false;
-            }
-
-            static MethodInfo[]? GetDeclaredMethods(Type type) =>
-                type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-
-            var methods = GetDeclaredMethods(parentType);
-            if (methods == null)
-            {
-                return false;
-            }
-
-            foreach (MethodInfo candidateMethod in methods)
-            {
-                var attributes = candidateMethod.GetCustomAttributes<StateMachineAttribute>(inherit: false);
-                // ReSharper disable once ConditionIsAlwaysTrueOrFalse - Taken from CoreFX
-                if (attributes is null)
-                {
-                    continue;
-                }
-
-                bool foundAttribute = false, foundIteratorAttribute = false;
-                foreach (StateMachineAttribute asma in attributes)
-                {
-                    if (asma.StateMachineType == declaringType)
-                    {
-                        foundAttribute = true;
-                        foundIteratorAttribute |= asma is IteratorStateMachineAttribute || asma is AsyncIteratorStateMachineAttribute;
-                    }
-                }
-
-                if (foundAttribute)
-                {
-                    // If this is an iterator (sync or async), mark the iterator as changed, so it gets the + annotation
-                    // of the original method. Non-iterator async state machines resolve directly to their builder methods
-                    // so aren't marked as changed.
-                    method = candidateMethod;
-                    declaringType = candidateMethod.DeclaringType!;
-                    return foundIteratorAttribute;
-                }
-            }
-
-            return false;
         }
     }
 }
